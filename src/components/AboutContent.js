ae40474b6a6bef5bb5e2172f600baa484284d77b
anirudhys123 <saianirudh643@gmail.com>
2025-01-26 17:52:47 +0530

    removed college

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -9,7 +9,11 @@ const AboutContent = () => {
     <div className="about">
         <div className="left">
             <h1><u>My Education</u> </h1> 
+<<<<<<< HEAD
             <p class = "edu">Iam currently pursuing Integrated M.Tech and currently maintaining gpa of 7.6 till 5th year 1st semester.</p> 
+=======
+            <p class = "edu">Iam currently pursuing Integrated M.Tech and currently maintaining gpa of 7.8 till 5th year 1st semester.</p> 
+>>>>>>> 458cef4 (removed college)
             <h1><u>My Skills</u> </h1> 
             <p>Languages/Softwares : C, JAVA, Python, Ansys, Matlab.</p> 
             <p>Web Technologies : Html, CSS, JavaScript, React.</p>
