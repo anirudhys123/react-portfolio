c72debfa5f88a1489a1a36a08b2cf60cbc183d11
anirudhys123 <saianirudh643@gmail.com>
2025-03-12 21:40:05 +0530

    new change

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -2,31 +2,21 @@ import "./AboutContentStyles.css";
 
 import React from 'react';
 
-
-
 const AboutContent = () => {
   return (
     <div className="about">
         <div className="left">
             <h1><u>My Education</u> </h1> 
-<<<<<<< HEAD
-            <p class = "edu">Iam currently pursuing Integrated M.Tech and currently maintaining gpa of 7.6 till 5th year 1st semester.</p> 
-=======
-            <p class = "edu">Iam currently pursuing Integrated M.Tech and currently maintaining gpa of 7.8 till 5th year 1st semester.</p> 
->>>>>>> 458cef4 (removed college)
+            <p class="edu">I am currently pursuing Integrated M.Tech and currently maintaining GPA of 7.8 till 5th year 1st semester.</p> 
             <h1><u>My Skills</u> </h1> 
-            <p>Languages/Softwares : C, JAVA, Python, Ansys, Matlab.</p> 
-            <p>Web Technologies : Html, CSS, JavaScript, React.</p>
-            <p> Data Bases : MongoDB</p>
-            <p> Developer Tools : VS Code , Github</p>
-            <p>Strengths : Dedication, Sincerity, Honesty, Punctuality.</p>
-
+            <p>Languages/Softwares: C, JAVA, Python, Ansys, Matlab.</p> 
+            <p>Web Technologies: Html, CSS, JavaScript, React.</p>
+            <p>Data Bases: MongoDB</p>
+            <p>Developer Tools: VS Code, Github</p>
+            <p>Strengths: Dedication, Sincerity, Honesty, Punctuality.</p>
         </div>
         <div className="right">
-          
-
         </div>
-      
     </div>
   )
 }
