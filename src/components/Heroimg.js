474800c12b0ab0460b2a880c5759cea543e4e156
anirudhys123 <saianirudh643@gmail.com>
2025-03-13 18:37:45 +0530

    modified header

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -12,7 +12,8 @@ const HeroImg = () => {
 
     </div> 
     <div className="content" >
-        <h1>Full Stack Web / App Developer</h1>
+    <h1>Mechanical Engineer && MERN Stack Developer</h1>
+
       <div style={{marginLeft:'-35px'}}>
         <Link to="/project" className="btn">my work</Link>
         <Link to="/contact" className="btn btn-light"> Contact</Link>
