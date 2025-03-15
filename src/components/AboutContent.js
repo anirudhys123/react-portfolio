7405a38bca20f8e05cc9ee50cc9fb6153c270eaf
anirudhys123 <saianirudh643@gmail.com>
2025-03-15 13:18:42 +0530

    added skills frontend

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -10,7 +10,7 @@ const AboutContent = () => {
             <p class="edu">I am currently pursuing Integrated M.Tech and currently maintaining GPA of 8/10 till 5th year 1st semester.</p> 
             <h1><u>My Skills</u> </h1> 
             <p>Languages/Softwares: C, JAVA, Python, Ansys, Matlab.</p> 
-            <p> Frontend Technologies: Html, CSS, JavaScript, React.</p>
+            <p> Frontend Technologies: Html, CSS, JavaScript, TypeScript, Bootstrap, React, Material UI.</p>
             <p>Data Bases: MongoDB , SQL</p>
             <p> Back end Technologies : Node JS , Express JS</p>
             <p>Developer Tools: VS Code, Github</p>
