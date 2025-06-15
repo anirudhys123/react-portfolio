d4ac4fc26b2e67b59349e12d597921daccc97d94
anirudhys123 <saianirudh643@gmail.com>
2025-06-15 19:18:05 +0530

    new

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,27 +1,25 @@
 import "./HeroImgStyles.css";
 
-import React from 'react'
-import IntroImg from "../assests/ai.jpg"
-import {Link} from "react-router-dom";
+import React from 'react';
+import IntroImg from "../assests/ai.jpg";
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
-     <div className="mask">
-     <img className="into-img"  src ={IntroImg} alt="IntroImg"/>
-
-    </div> 
-    <div className="content" >
-    <h1>UI/ UX Designer && MERN Stack Developer</h1>
-
-      <div style={{marginLeft:'-35px'}}>
-        <Link to="/project" className="btn">my work</Link>
-        <Link to="/contact" className="btn btn-light"> Contact</Link>
+      <div className="mask">
+        <img className="into-img" src={IntroImg} alt="IntroImg" />
       </div>
-
+      <div className="content">
+        <h1>Mechanical Engineer && MERN Stack Developer</h1>
+        <div style={{ marginLeft: '-35px' }}>
+          <Link to="/project" className="btn">my work</Link>
+          <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default HeroImg
+export default HeroImg;
+
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -15,29 +15,31 @@ import pro15 from "../assests/grocery.png"
 import pro16 from "../assests/sfd_bmd.png"
 import pro17 from "../assests/Input.jpg"
 import pro18 from "../assests/unit.png"
+import pro19 from "../assests/chat-bot.png"
 
 
 const ProjectCardData = [
 
-  {
-    imgsrc: pro1,
-    title: "DRDO Project-Intern",
-    text: "Using Python, NumPy, and SciPy, I enhanced aerodynamic heating assessments for MIRVs at DRDL. And also improved efficiency and adaptability, and precision over earlier MATLAB code, and thoroughly studied the concepts of heat transfer and aerodynamics",
-    view: "https://drive.google.com/file/d/17IOZHuCmrEWbY_7tBJClT5CRmzrJOS8T/view?usp=drive_link",
+   {
+    imgsrc: pro19,
+    title: "AI - Chat_bot",
+    text: "The UI of this was made using HTML , CSS , JavaScript and NLP's are used to make the conversations realistic. The project is deployed on the netlify",
+    view: "https://anirudh-chat-bot.netlify.app/",
   },
 
-  {
-    imgsrc: pro2,
-    title: "Mini Project at Indian Railways",
-    text: "Explored various components of an IC Engine and studied them in a detailed manner.",
-    view: "https://drive.google.com/file/d/12_O2m0lteulBwN2pQ9gW2FB0PTJBDkBW/view?usp=drive_link",
-  },
 
   {
-    imgsrc: pro18,
-    title: "Engineering Unit Converter",
-    text: "Developed a responsive web-based Engineering Unit Converter using React JS for accurate, real-time conversions of length, area, volume, temperature, pressure, and mass. Designed to assist mechanical engineering students in simplifying design tasks, lab work, and academic projects by minimizing manual errors and saving time.",
-    view: "https://jntuh-mechanical.vercel.app/",
+    imgsrc: pro13,
+    title:  "Food Delivery Website",
+    text: "This website was made by using React JS and deployed at Vercel. From this the user can order food items at his/her location. It is not responsive now so kindly view it on laptop only.",
+    view: "https://anirudh-foods.vercel.app/",
+  },
+  
+  {
+    imgsrc: pro15,
+    title: "Grocery Website",
+    text: "This website was made by using React JS and deployed at Netlify. From this the user can order fruits, vegetables, etc. It is not responsive now so kindly view it on laptop only.",
+    view: "https://anirudh-grocery.netlify.app/",
   },
 
   {
@@ -61,19 +63,28 @@ const ProjectCardData = [
     view: "https://anirudh-saloons.vercel.app/",
   },
 
-  {
-    imgsrc: pro13,
-    title:  "Food Delivery Website",
-    text: "This website was made by using React JS and deployed at Vercel. From this the user can order food items at his/her location. It is not responsive now so kindly view it on laptop only.",
-    view: "https://anirudh-foods.vercel.app/",
+   {
+    imgsrc: pro1,
+    title: "DRDO Project-Intern",
+    text: "Using Python, NumPy, and SciPy, I enhanced aerodynamic heating assessments for MIRVs at DRDL. And also improved efficiency and adaptability, and precision over earlier MATLAB code, and thoroughly studied the concepts of heat transfer and aerodynamics",
+    view: "https://drive.google.com/file/d/17IOZHuCmrEWbY_7tBJClT5CRmzrJOS8T/view?usp=drive_link",
   },
+
+  {
+    imgsrc: pro2,
+    title: "Mini Project at Indian Railways",
+    text: "Explored various components of an IC Engine and studied them in a detailed manner.",
+    view: "https://drive.google.com/file/d/12_O2m0lteulBwN2pQ9gW2FB0PTJBDkBW/view?usp=drive_link",
+  },
+
+  {
+    imgsrc: pro18,
+    title: "Engineering Unit Converter",
+    text: "Developed a responsive web-based Engineering Unit Converter using React JS for accurate, real-time conversions of length, area, volume, temperature, pressure, and mass. Designed to assist mechanical engineering students in simplifying design tasks, lab work, and academic projects by minimizing manual errors and saving time.",
+    view: "https://jntuh-mechanical.vercel.app/",
+  },
+
   
-  {
-    imgsrc: pro15,
-    title: "Grocery Website",
-    text: "This website was made by using React JS and deployed at Netlify. From this the user can order fruits, vegetables, etc. It is not responsive now so kindly view it on laptop only.",
-    view: "https://anirudh-grocery.netlify.app/",
-  },
   
   // {
   //   imgsrc: pro4,
