ce6da2f94586b99665b4f847502f855c97c55d0e
anirudhys123 <saianirudh643@gmail.com>
2026-02-17 20:58:42 +0530

    removed hvac

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,17 +8,17 @@ const AboutContent = () => {
         <div className="left">
   <h1><u>My Education</u></h1>
  <p className="edu">
-  B.Tech (Mechanical) and M.Tech (Thermal) from JNTU Hyderabad with 8.5 CGPA; currently working as an HVAC Site Engineer at CtrlS Data Center, Navi Mumbai.
+  B.Tech (Mechanical) and M.Tech (Thermal) from JNTU Hyderabad with 8.5 CGPA; currently working at CtrlS Data Center, Navi Mumbai.
 </p>
 
 
   <h1><u>My Skills</u></h1>
 
-<p><b>HVAC Systems:</b> Chilled Water, VRF/VRV, AHU, FCU</p>
+{/* <p><b>HVAC Systems:</b> Chilled Water, VRF/VRV, AHU, FCU</p>
 <p><b>Site Work:</b> HVAC installation, ducting, piping & insulation</p>
 <p><b>Drawings & Testing:</b> Shop/GA drawings, pressure testing, flushing</p>
 <p><b>Coordination & Safety:</b> Client & MEP coordination, QA/QC, site safety</p>
-<p><b>Software (HVAC):</b> AutoCAD, Revit MEP, MS Excel</p>
+<p><b>Software (HVAC):</b> AutoCAD, Revit MEP, MS Excel</p> */}
 
 <p><b>Web Development:</b> HTML, CSS, JavaScript, TypeScript, React</p>
 <p><b>Backend & DB:</b> Node.js, Express.js, MongoDB, SQL</p>
