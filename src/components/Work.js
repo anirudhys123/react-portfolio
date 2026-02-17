c452b9f3304fd345664165ce7bd8f425895d754d
anirudhys123 <saianirudh643@gmail.com>
2026-02-17 21:10:25 +0530

    y

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,32 +1,29 @@
 import "./WorkCardStyles.css";
 import WorkCard from "./WorkCard";
 import WorkCardData from "./WorkCardData";
-
-import React from 'react';
+import React from "react";
 
 const Work = () => {
   return (
     <div className="work-container">
-    <h1 className="project-heading"></h1>
-    <div className="project-container">
-        {WorkCardData.map((val,ind) =>{
-            return (
-                <WorkCard 
-                key = {ind}
-                imgsrc = {val.imgsrc}
-                title = {val.title}
-                view = {val.view}
-                text = {val.text}
-                source = {val.source}
-                />
-            )
+      <h1 className="project-heading">My Projects</h1>
 
-
-            
+      <div className="project-container">
+        {WorkCardData.map((val, ind) => {
+          return (
+            <WorkCard
+              key={ind}
+              imgsrc={val.imgsrc}
+              title={val.title}
+              view={val.view}
+              text={val.text}
+              source={val.source}
+            />
+          );
         })}
-     </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Work;
--- src/components/Work1.js
+++ src/components/Work1.js
@@ -1,32 +1,29 @@
 import "./WorkCardStyles.css";
 import WorkCard from "./WorkCard";
 import WorkData from "./WorkCardData1";
-
-import React from 'react';
+import React from "react";
 
 const Work1 = () => {
   return (
     <div className="work-container">
-    <h1 className="project-heading"> </h1>
-    <div className="project-container">
-        {WorkData.map((val,ind) =>{
-            return (
-                <WorkCard 
-                key = {ind}
-                imgsrc = {val.imgsrc}
-                title = {val.title}
-                view = {val.view}
-                text = {val.text}
-                source = {val.source}
-                />
-            )
+      <h1 className="project-heading">My Certifications</h1>
 
-
-            
+      <div className="project-container">
+        {WorkData.map((val, ind) => {
+          return (
+            <WorkCard
+              key={ind}
+              imgsrc={val.imgsrc}
+              title={val.title}
+              view={val.view}
+              text={val.text}
+              source={val.source}
+            />
+          );
         })}
-     </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Work1;
