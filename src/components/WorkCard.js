5627e9d0bcaed3bfaea9cfbd6376a6957bb588f0
anirudhys123 <saianirudh643@gmail.com>
2026-02-17 21:14:39 +0530

    final

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,23 +1,25 @@
 import "./WorkCardStyles.css";
-
-import React from 'react';
-
+import React from "react";
 import { Link } from "react-router-dom";
 
 const WorkCard = (props) => {
   return (
     <div className="project-card">
-         <img src ={props.imgsrc} alt="image" />
-        <h2 className="project-title">{props.title}</h2>
-        <div className="pro-details">
-            <p> {props.text}</p>
-        </div>
-        <div className="pro-btns">
-            <Link to ={props.view} className="btn"> View</Link>
-        </div>
-        
-        </div>
-  )
-}
+      <img src={props.imgsrc} alt={props.title} />
+
+      <h2 className="project-title">{props.title}</h2>
+
+      <div className="pro-details">
+        <p>{props.text}</p>
+      </div>
+
+      <div className="pro-btns">
+        <Link to={props.view} className="btn">
+          View
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default WorkCard;
